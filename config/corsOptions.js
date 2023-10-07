73531cdd233c51c18f1aa420fa8620d3876e4e4d
zakaryaalgeria <zz194451@gmail.com>
2023-10-07 10:33:49 +0100

    change the cors option

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://junction-hackathon-backend.vercel.app',
     optionsSuccessStatus: 200 
 }
 module.exports = corsOptions
